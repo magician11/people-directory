5ee84260d58794ee7bd9580ced26a5f7455c3301
Andrew Golightly <andrew@golightlyplus.com>
2015-12-02 14:00:08 +0700

    basic editing works

--- src/js/EditPerson.jsx
+++ src/js/EditPerson.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Firebase from 'firebase';
+import { Input, Grid, Row, Col, ButtonInput, Alert } from 'react-bootstrap';
 
 var EditPerson = React.createClass({
 
@@ -18,6 +19,11 @@ var EditPerson = React.createClass({
       formSubmitted: false
     };
   },
+  handleInputChange: function(e) {
+    var nextState = this.state.person;
+    nextState[e.target.name] = e.target.value;
+    this.setState({person: nextState});
+  },
   componentWillMount: function() {
     this.ref = new Firebase("https://people-directory.firebaseio.com/baptiste/" + this.props.params.id);
     this.ref.on('value', function(data) {
@@ -32,12 +38,19 @@ var EditPerson = React.createClass({
     var person = {};
 
     // add all input refs to an object and save to database
-    Object.keys(this.refs).forEach(function(inputFieldRef) {
-      person[inputFieldRef] = this.refs[inputFieldRef].getValue();
-    }.bind(this));
-
+    // Object.keys(this.refs).forEach(function(inputFieldRef) {
+    //   person[inputFieldRef] = this.refs[inputFieldRef].getValue();
+    // }.bind(this));
+    console.log(this.state.person);
     //var ref = new Firebase("https://people-directory.firebaseio.com/baptiste");
-    this.ref.update(person);
+    console.log('updating firebase');
+    //console.log(this.ref);
+    this.ref.set(this.state.person);
+    this.ref.on('value', function(data) {
+
+      console.log(data.val());
+
+    });
 
     //
     // var fileReader = new FileReader();
@@ -55,26 +68,24 @@ var EditPerson = React.createClass({
     this.setState({
       formSubmitted: true
     });
-
-    return;
   },
   render: function() {
 
     if(!this.state.formSubmitted) {
       var form = <form onSubmit={this.handleSubmit}>
-        <Input type="text" label="First Name" ref="firstName" value={this.state.person.firstName} required />
-        <Input type="text" label="Last Name" ref="lastName" value={this.state.person.lastName} required />
-        <Input type="file" label="Profile photo" accept="image/*" ref="image" required />
-        <Input type="text" label="City" value={this.state.person.city} ref="city" required />
-        <Input type="text" label="State/Province" value={this.state.person.state} ref="state" required />
-        <Input type="select" label="Country" value={this.state.person.country} required >
+        <Input type="text" label="First Name" name="firstName" value={this.state.person.firstName} onChange={this.handleInputChange} required />
+        <Input type="text" label="Last Name" name="lastName" value={this.state.person.lastName} onChange={this.handleInputChange} required />
+
+        <Input type="text" label="City" value={this.state.person.city} name="city" onChange={this.handleInputChange} required />
+        <Input type="text" label="State/Province" value={this.state.person.state} name="state" onChange={this.handleInputChange} required />
+        <Input type="select" label="Country" value={this.state.person.country}  onChange={this.handleInputChange} required >
           <option value="USA">USA</option>
           <option value="Canada">Canada</option>
           <option value="Nigeria">Nigeria</option>
         </Input>
-        <Input type="text" label="Studio" value={this.state.person.studioName} ref="studioName" required />
-        <Input type="url" label="Studio Website" value={this.state.person.studioURL} ref="studioURL" required />
-        <Input type="textarea" label="Description" value={this.state.person.description} ref="description" required />
+        <Input type="text" label="Studio" value={this.state.person.studioName} name="studioName" onChange={this.handleInputChange} required />
+        <Input type="url" label="Studio Website" value={this.state.person.studioURL} name="studioURL" onChange={this.handleInputChange} required />
+        <Input type="textarea" label="Description" value={this.state.person.description} name="description" onChange={this.handleInputChange} required />
         <ButtonInput type="submit" value="Save" bsStyle="primary" bsSize="large" className="center-block" />
       </form>;
     }
